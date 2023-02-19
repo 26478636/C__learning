61301cf4cf6a3751c9e769a7d331a3da0fc5cc23
26478636 <2647863677@qq.com>
2023-02-19 19:12:04 +0800

    提交

---
+++ 4.封装总结/new&delete.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+//重载new
+void* operator new(size_t size){
+    cout<<"创建内存空间"<<endl;
+    void* p=malloc(size);
+    return p;
+}
+//重载delete
+void operator delete(void* p){
+    cout<<"删除内存空间"<<endl;
+    if(p){
+        free(p);
+    }
+}
+
+class A{
+public:
+    A(){
+        cout<<"默认构造"<<endl;
+    }
+    ~A(){
+        cout<<"析构函数"<<endl;
+    }
+};
+
+char arr[100];
+int main(){
+    A* a1=new A();
+    delete a1;
+
+    A* a2=new(arr) A();
+    A* a3=new(arr+sizeof(A)) A();
+    return 0;
+}
