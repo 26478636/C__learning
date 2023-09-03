1ae919b6947d36414d73f03de9e8d77dbfca989f
2647863677 <2647863677@qq.com>
2023-09-03 15:05:17 +0800

    提交

--- 5.多态--重载/练习1.cpp
+++ 5.多态--重载/练习1.cpp
@@ -2,92 +2,109 @@
 
 using namespace std;
 
-//Fraction 类
-class Fraction{
-    //成员属性
-    private:
-    int _x,_y;
-    //构造函数
-    public:
-    Fraction(int x,int y):_x(x),_y(y){
-
+// Fraction 类
+class Fraction
+{
+    // 成员属性
+private:
+    int _x, _y;
+    // 构造函数
+public:
+    Fraction(int x, int y) : _x(x), _y(y)
+    {
     }
-    //功能函数
-    //最大公约数
-    int measure(int x,int y){
-        int z=y;
-        while(x%y!=0){
-            z=x%y;
-            x=y;
-            y=z;
+    // 功能函数
+    // 最大公约数
+    int measure(int x, int y)
+    {
+        int z = y;
+        while (x % y != 0)
+        {
+            z = x % y;
+            x = y;
+            y = z;
         }
         return z;
     }
-    //f1+f2
-    Fraction operator+(const Fraction& f){
-        int x=this->_x*f._y+this->_y*f._x;
-        int y=this->_y*f._y;
-        int z=measure(x,y);
-        return Fraction(x/z,y/z);
-    } 
-    //f1*f2
-    Fraction operator*(const Fraction& f){
-        int x=this->_x*f._x;
-        int y=this->_y*f._y;
-        int z=measure(x,y);
-        return Fraction(x/z,y/z);
+    // f1+f2
+    Fraction operator+(const Fraction &f)
+    {
+        int x = this->_x * f._y + this->_y * f._x;
+        int y = this->_y * f._y;
+        int z = measure(x, y);
+        return Fraction(x / z, y / z);
     }
-    Fraction& operator-(){
-        this->_x=-this->_x;
+    // f1*f2
+    Fraction operator*(const Fraction &f)
+    {
+        int x = this->_x * f._x;
+        int y = this->_y * f._y;
+        int z = measure(x, y);
+        return Fraction(x / z, y / z);
+    }
+    // 这是个负号
+    Fraction &operator-()
+    {
+        this->_x = (-this->_x);
         return *this;
     }
-    Fraction& operator+=(const Fraction& f){
-        int x=this->_x*f._y+this->_y*f._x;
-        int y=this->_y*f._y;
-        int z=measure(x,y);
-        this->_x=x/z;
-        this->_y=y/z;
+    Fraction &operator+=(const Fraction &f)
+    {
+        int x = this->_x * f._y + this->_y * f._x;
+        int y = this->_y * f._y;
+        int z = measure(x, y);
+        this->_x = x / z;
+        this->_y = y / z;
         return *this;
     }
-    Fraction& operator*=(const Fraction& f){
-        int x=this->_x*f._x;
-        int y=this->_y*f._y;
-        int z=measure(x,y);
-        this->_x=x/z;
-        this->_y=y/z;
+    Fraction &operator*=(const Fraction &f)
+    {
+        int x = this->_x * f._x;
+        int y = this->_y * f._y;
+        int z = measure(x, y);
+        this->_x = x / z;
+        this->_y = y / z;
         return *this;
     }
-    bool operator==(const Fraction& f){
-        return this->_x*f._y==this->_y*f._x;
+    bool operator==(const Fraction &f)
+    {
+        return this->_x * f._y == this->_y * f._x;
     }
-    bool operator<(const Fraction& f){
-        return this->_x*f._y<this->_y*f._x;
+    bool operator<(const Fraction &f)
+    {
+        return this->_x * f._y < this->_y * f._x;
     }
-    bool operator>(const Fraction& f){
-        return this->_x*f._y>this->_y*f._x;
+    bool operator>(const Fraction &f)
+    {
+        return this->_x * f._y > this->_y * f._x;
     }
-    //功能输出
-    public:
-    friend ostream& operator<<(ostream& os,const Fraction& f){
-        os<<f._x<<"/"<<f._y<<endl; 
+    // 功能输出
+public:
+    friend ostream &operator<<(ostream &os, const Fraction &f)
+    {
+        os << f._x << "/" << f._y << endl;
         return os;
     }
 };
-int main(){
-    Fraction f1{1,4},f2{1,6};
-    cout<<(f1+f2)<<endl;
-    cout<<(f1*f2)<<endl;
-    cout<<(-f1)<<endl;
-    cout<<(f1+=f2)<<endl;
-    cout<<(f1*=f2)<<endl;
-    if(f1==f2){
-        cout<<"相等"<<endl;
+int main()
+{
+    Fraction f1{1, 4}, f2{1, 6};
+    cout << (f1 + f2) << endl;
+    cout << (f1 * f2) << endl;
+    cout << (-f1) << endl;
+    cout << (f1 += f2) << endl;
+    cout << (f1 *= f2) << endl;
+    if (f1 == f2)
+    {
+        cout << "相等" << endl;
     }
-    if(f1<f2){
-        cout<<"小于"<<endl;
+    if (f1 < f2)
+    {
+        cout << "小于" << endl;
     }
-    if(f1>f2){
-        cout<<"大于"<<endl;
+    if (f1 > f2)
+    {
+        cout << "大于" << endl;
     }
     return 0;
 }
--- 5.多态--重载/运算符重载.cpp
+++ 5.多态--重载/运算符重载.cpp
@@ -2,65 +2,74 @@
 
 using namespace std;
 
-//Point 类
-class Point{
-    //成员属性
-    private:
-    float _x,_y;
-    //构造函数
-    public:
-    Point():_x(0),_y(0){
-
+// Point 类
+class Point
+{
+    // 成员属性
+private:
+    float _x, _y;
+    // 构造函数
+public:
+    Point() : _x(0), _y(0)
+    {
     }
-    Point(float x,float y):_x(x),_y(y){
-
+    Point(float x, float y) : _x(x), _y(y)
+    {
     }
-    //成员函数
-    //p1+p2
-    Point operator+(const Point& p){
-        return Point(this->_x+=p._x,this->_y+=p._y);
+    // 成员函数
+    // p1+p2
+    Point operator+(const Point &p)
+    {
+        return Point(this->_x += p._x, this->_y += p._y);
     }
-    //后置p1++
-    Point operator++(int){
-        Point p(this->_x+=p._x,this->_y+=p._y);
+    // 后置p1++
+    Point operator++(int)
+    {
+        Point p(this->_x += p._x, this->_y += p._y);
         this->_x++;
         this->_y++;
         return p;
     }
-    //前置++p1
-    Point operator++(){
+    // 前置++p1
+    Point operator++()
+    {
         this->_x++;
         this->_y++;
         return *this;
     }
-    //p1+=p2
-    Point operator+=(const Point& p){
-        this->_x+=p._x;
-        this->_y+=p._y;
+    // p1+=p2
+    Point operator+=(const Point &p)
+    {
+        this->_x += p._x;
+        this->_y += p._y;
         return *this;
     }
-    //p1*p2
-    float operator*(const Point& p){
-        return this->_x*p._x+this->_y*p._y;
+    // p1*p2
+    float operator*(const Point &p)
+    {
+        return this->_x * p._x + this->_y * p._y;
     }
-    
-    //友元函数
-    friend ostream& operator<<(ostream& os,const Point& p);  
+
+    // 友元函数
+    friend ostream &operator<<(ostream &os, const Point &p);
 };
 
-//实现功能
-ostream& operator<<(ostream& os,const Point& p){
-    os<<"x="<<p._x<<"......"<<"y="<<p._y<<endl;
+// 实现功能
+ostream &operator<<(ostream &os, const Point &p)
+{
+    os << "x=" << p._x << "......"
+       << "y=" << p._y << endl;
     return os;
 }
 
-int main(){
-    Point p1{1.2f,4.3f},p2{2.3f,3.3f};
-    cout<<(p1+p2)<<endl;
-    cout<<p1++<<endl;
-    cout<<++p1<<endl;
-    cout<<(p1+=p2)<<endl;
-    cout<<p1*p2<<endl;
-    cout<<p1<<endl;
+int main()
+{
+    Point p1{1.2f, 4.3f}, p2{2.3f, 3.3f};
+    cout << (p1 + p2) << endl;
+    cout << p1++ << endl;
+    cout << ++p1 << endl;
+    cout << (p1 += p2) << endl;
+    cout << p1 * p2 << endl;
+    cout << p1 << endl;
     return 0;
 }
